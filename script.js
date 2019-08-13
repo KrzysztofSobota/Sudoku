4b5f2ec9f594d18a280057982b9018ff20f06a6f
KrzysztofSobota <ksobota1981@gmail.com>
2019-08-13 23:45:58 +0200

    CSS and JS change (graphic file replace)
    CSS:
    - mediaquery range resolutions change,
    - colors replace,
    - 'class' names add/change,
    - flexboxes parameters change,
    
    JS:
    - add show/hide option (click button)

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 'use strict';
-window.addEventListener("DOMContentLoaded", Sudoku);
+window.addEventListener('DOMContentLoaded', Sudoku);
 
 function Sudoku() {
   let game = document.querySelector('.gamespace');
 
   /* Fill gameboard with 81 (9x9) small squares */
   for (let j = 80; j >= 0; j--) {
-    game.insertAdjacentHTML('afterbegin', `<div class="squares" id="num${j}"><span class="num"></span></div>`);
+    game.insertAdjacentHTML('afterbegin',`<div class="squares" id="num${j}"><span class="num"></span></div>`);
   }
 
   let square = document.querySelectorAll('.squares');
 
   /* 4 red lines divide board into 9 squares */
   function redLines() {
-    for (let i = 2; i <= 81;) {
+    for (let i = 2; i <= 81; ) {
       square[i].style.borderRight = '2px solid red';
       square[i + 3].style.borderRight = '2px solid red';
       square[i + 1].style.borderLeft = '2px solid red';
@@ -28,7 +28,7 @@ function Sudoku() {
       square[i].style.borderTop = '2px solid red';
       square[i + 27].style.borderTop = '2px solid red';
     }
-  };
+  }
 
   redLines();
 
@@ -38,7 +38,7 @@ function Sudoku() {
   function sorting() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let n = numbers.length;
-    for (let i = 1; i <= n;) {
+    for (let i = 1; i <= n; ) {
       let a = Math.floor(Math.random() * n);
       let b = numbers[a];
 
@@ -50,7 +50,6 @@ function Sudoku() {
 
     return sortArray;
   }
-
 
   //  Make 27 part of small arrays
   let fullArray = [];
@@ -67,15 +66,13 @@ function Sudoku() {
     let part3 = [i7, i8, i9];
 
     /* All other part(4-9) are made from part(1-3) by moving the last element in array to begin (like [a,b,c] -> [c,a,b] -> [b,c,a]). */
-    let part4 = [i9, i7, i8];
-    let part5 = [i3, i1, i2];
-    let part6 = [i6, i4, i5];
-    let part7 = [i5, i6, i4];
-    let part8 = [i8, i9, i7];
-    let part9 = [i2, i3, i1];
+    let part4 = [i9, i7, i8]; let part5 = [i3, i1, i2]; let part6 = [i6, i4, i5];
+    let part7 = [i5, i6, i4]; let part8 = [i8, i9, i7]; let part9 = [i2, i3, i1];
 
     /* The right order of inserting every part into 9x9 sudoku array (fullArray) */
-    fullArray = [...part1, ...part2, ...part3, ...part4, ...part5, ...part6, ...part7, ...part8, ...part9, ...part8, ...part9, ...part7, ...part2, ...part3, ...part1, ...part5, ...part6, ...part4, ...part6, ...part4, ...part5, ...part9, ...part7, ...part8, ...part3, ...part1, ...part2];
+    fullArray = [...part1, ...part2, ...part3, ...part4, ...part5, ...part6, ...part7, ...part8, ...part9,
+                ...part8, ...part9, ...part7, ...part2, ...part3, ...part1, ...part5, ...part6, ...part4,
+                ...part6, ...part4, ...part5, ...part9, ...part7, ...part8, ...part3, ...part1, ...part2];
 
     return fullArray;
   }
@@ -83,13 +80,13 @@ function Sudoku() {
   arrayParts();
 
   //  Check correct sum for all digits and 2 main diagonals before inside them on the board
-  let sumDigits = fullArray.reduce(function (previousValue, currentValue, index, array) {
+  let sumDigits = fullArray.reduce(function(previousValue, currentValue, index, array) {
     return previousValue + currentValue;
   });
   if (sumDigits != 405) {
     alert('Wrong digits!');
-  };
-  let mainDiagonals = function () {
+  }
+  let mainDiagonals = function() {
     let leftResult = 0;
     let rightResult = 0;
     for (let i = 0; i <= 80; i += 10) {
@@ -101,9 +98,8 @@ function Sudoku() {
 
     if (leftResult != 45 || rightResult != 45) {
       alert('Wrong digits!');
-    };
+    }
   };
-
 
   /* Selected number of digits (game level type) */
 
@@ -119,14 +115,12 @@ function Sudoku() {
   let n = selectedValues();
   listGroup.addEventListener('change', selectedValues);
 
-
   /* Put selected number of digits (n) into the board */
   let noRepeatArray = [];
   let indexArray = Array.from(fullArray); // copy of 'fullArray' to make the operations on it
 
   /* Taking index numbers - it is n-indexes loop, where 'n' depends on game level */
   let indexNumber = undefined;
-
 
   function insertDigits(n) {
     let tempArray = [];
@@ -140,9 +134,7 @@ function Sudoku() {
 
       noRepeatArray = [...new Set(tempArray)];
       k = noRepeatArray.length;
-    }
-    while (k < n);
-
+    } while (k < n);
   }
   console.log(indexArray);
   insertDigits(n);
@@ -154,7 +146,7 @@ function Sudoku() {
 
   function fill() {
     for (let i = 0; i < n; i++) {
-      square[noRepeatIndexes[i]].insertAdjacentHTML('afterbegin', `<input type="text" class="digits" id="pos${noRepeatIndexes[i]}" maxlength="1" size="2" value="">`);
+      square[noRepeatIndexes[i]].insertAdjacentHTML('afterbegin',`<input type="text" class="digits" id="pos${noRepeatIndexes[i]}" maxlength="1" size="2" value="">`);
     }
   }
 
@@ -168,8 +160,7 @@ function Sudoku() {
 
   fill();
 
-  indexNumber == undefined ? game.src = '/example.svg' : fillBoard();
-
+  indexNumber == undefined ? (game.src = '/example(mini).svg') : fillBoard();
 
   const getNumber = str => Number(str.match(/\d+/)[0]);
   const num = event => getNumber(event.target.id);
@@ -200,43 +191,48 @@ function Sudoku() {
 
   compareDigit();
 
-
   /* 
   game.addEventListener('click', compareDigit, false);
   */
 }
 
-
 /* Sudoku start only when type and level of game are checked */
 let btnStart = document.querySelector('#play');
 let btnReset = document.querySelector('#reset');
 
-
 /* Timer */
 const timeIntervals = [
   /* all in milliseconds */
-  ["hour", 3600000],
-  ["minute", 60000],
-  ["second", 1000]
+  ['hour', 3600000],
+  ['minute', 60000],
+  ['second', 1000]
 ];
-const tick = (start) => () => {
+const tick = start => () => {
   let elapsed = Date.now() - start;
+  let newRecord = document.querySelector('#new-record');
 
   for (let [unit, ms] of timeIntervals) {
     let timing = Math.floor(elapsed / ms);
-    let param = document.getElementById(unit);
+    let timerParam = document.getElementById(unit);
+    let recordParam = document.getElementsByClassName(unit);
 
-    if (typeof (timing) == "number") {
-      timing < 10 ? param.textContent = `0${timing}` : param.textContent = `${timing}`;
+    if (typeof timing == 'number') {
+      // 00-09 or 10-59
+      timing < 10
+        ? (timerParam.textContent = `0${timing}`)
+        : (timerParam.textContent = `${timing}`);
     }
+
+    /* if (recordParam == '--') {
+      newRecord.style.display = 'table-cell';
+      newRecord.innerHTML = 'NEW RECORD !!!';
+    } */
 
     elapsed %= ms;
   }
 };
 
-btnStart.addEventListener('click', toggleTimer, {
-  once: true
-});
+btnStart.addEventListener('click', toggleTimer, { once: true });
 btnReset.addEventListener('click', toggleTimer);
 
 // Check clicked type of radiobox
@@ -246,30 +242,29 @@ let record = document.getElementById('timeOn');
 function toggleTimer(event) {
   let button = event.target.id;
 
-  if (button = 'play' && record.checked == true) {
+  if ((button = 'play' && record.checked == true)) {
     window.setInterval(tick(Date.now()), 1000);
     // Sudoku();
     // insertDigits(n);
   }
 
-  if (button = 'reset') {
-    clearInterval(window.setInterval(tick()));
+  if ((button = 'reset' && record.checked == true)) {
+    clearInterval(tick(Date.now()));
   }
 }
-
 
 /* Remove selected options and make them defaults */
 btnReset.addEventListener('click', optionsClear);
 
 /* lables */
-const lables = document.querySelectorAll(".gameopt");
+const lables = document.querySelectorAll('.gameopt');
 
 /* radio */
-const radioButtons = document.querySelectorAll("[type='radio']");
+const radioButtons = document.querySelectorAll(`[type='radio']`);
 
 function radioReset() {
   lables.forEach(lable => {
-    lable.style.setProperty("--bg", "transparent");
+    lable.style.setProperty('--bg', 'transparent');
   });
   radioButtons.forEach(radioBtn => {
     radioBtn.checked = false;
@@ -277,7 +272,7 @@ function radioReset() {
 }
 
 /* select */
-const list = document.querySelector("#level-list");
+const list = document.querySelector('#level-list');
 
 function listReset() {
   list.selectedIndex = 0;
@@ -288,5 +283,26 @@ function optionsClear() {
   radioReset();
   listReset();
   /* default graphic on the board */
-  document.querySelector('.sudoku').style.background = 'url(/example.svg)';
+  document.querySelector('.sudoku').style.background =
+    'url(/example(mini).svg)';
 }
+
+/** User can hide theoretical description of "Sudoku". 
+This saves a lot of document space - it is especially important for mobile devices **/
+
+let showHideButton = document.querySelector('#savespace');
+let textChange = document.querySelector('.hide-show > span');
+
+function ShowHideText() {
+  let hiddenText = document.querySelector('.description');
+
+  if (hiddenText.style.display === 'none') {
+    hiddenText.style.display = 'flex';
+    textChange.innerHTML = 'Click to HIDE description';
+  } else {
+    hiddenText.style.display = 'none';
+    textChange.innerHTML = 'Click to SHOW description';
+  }
+}
+
+showHideButton.addEventListener('click', ShowHideText);
