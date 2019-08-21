93c44551964be158b23df1fe9ff1d02fff7f7358
KrzysztofSobota <ksobota1981@gmail.com>
2019-08-21 22:36:48 +0200

    Big JS code changes
    - reorder JS code elements (create board, click/change events, etc.)
    - change timer function,
    - more smaller functions,
    - make test function to check properly created digits in 'fullArray'

--- script.js
+++ script.js
@@ -1,14 +1,84 @@
 'use strict';
 // window.addEventListener('DOMContentLoaded', Gameboard);
 
+/**** Sudoku starts only when type and level of game are checked ****/
+
+/* STEP 1: Selecting game level difficulty (number of digits) */
+
+const listGroup = document.querySelector('#level-list');
+
+let selectedValues = () => Number(listGroup.value);
+let n = selectedValues();
+
+listGroup.addEventListener('change', selectedValues);
+
+/* STEP 2: Choose type of game (with/without timer) */
+
+const fun = document.querySelector('#timeOff');
+const record = document.querySelector('#timeOn');
+
+const btnStart = document.querySelector('#play');
+const btnReset = document.querySelector('#reset');
+btnStart.addEventListener('click', toggleTimer, { once: true });
+btnReset.addEventListener('click', toggleTimer);
+
+/* Timer */
+let timerId;
+
+const timeIntervals = [
+  /* all in milliseconds */
+  ['hour', 3600000],
+  ['minute', 60000],
+  ['second', 1000]
+];
+
+const tick = start => () => {
+  let elapsed = Date.now() - start;
+
+  for (let [unit, ms] of timeIntervals) {
+    let timing = Math.floor(elapsed / ms);
+    let timerParam = document.getElementById(unit);
+
+    // 00-09 or 10-59 numbers range
+    timerParam.innerHTML = (timing < 10 ? '0' : '') + timing;
+
+    elapsed %= ms;
+  }
+};
+
+function toggleTimer(event) {
+  let button = event.target.closest('button').id;
+
+  if (button === 'play' && record.checked == true) {
+    timerId = window.setInterval(tick(Date.now()), 1000);
+  }
+
+  if (button === 'reset' && record.checked == true) {
+    clearInterval(timerId);
+  }
+}
+
+
+/* Prepare gameboard */
+
+const game = document.querySelector('.gamespace');
+
+game.style.background = 'url(/example-mini.svg) no-repeat';
+game.style.backgroundSize = 'cover';
+
+if (n > 0) {
+  game.style.background = 'url()';
+  Gameboard();
+}
+
+
 /* Fill gameboard with 81 (9x9) small squares */
-function Gameboard() {
-  const game = document.querySelector('.gamespace');
-  for (let j = 0; j <= 80; j++) {
+function Gameboard() {  
+  for (let j = 0; j < 81; j++) {
     game.insertAdjacentHTML('afterbegin',`<div class="squares" id="num${80 - j}"><span class="num"></span></div>`);
   }
   
-  let square = document.querySelectorAll('.squares');
+  const square = document.querySelectorAll('.squares');
   
   /* 4 red lines divide board into 9 squares */
   function redLines() {  
@@ -32,135 +102,63 @@ function Gameboard() {
   redLines();
 }
 
-let area = Gameboard();
-
-/**** Sudoku starts only when type and level of game are checked ****/
-
-const btnStart = document.querySelector('#play');
-const btnReset = document.querySelector('#reset');
-
-/* STEP 1: Selecting number of digits (game level difficulty) */
-
-let listGroup = document.querySelector('#level-list');
-
-let selectedValues = () => Number(listGroup.value);
-let n = selectedValues();
-
-listGroup.addEventListener('change', selectedValues);
-
-/* STEP 2: Choose type of game (with/without timer) */
-
-btnStart.addEventListener('click', toggleTimer, { once: true });
-btnReset.addEventListener('click', toggleTimer);
-
-
-let fun = document.querySelector('#timeOff');
-let record = document.querySelector('#timeOn');
-
-
-function toggleTimer(event) {
-  let btn = event.target.id;
-  
-  if (btn = btnStart && record.checked == true) {
-    window.setInterval(tick(Date.now()), 1000);
-  }
-  if (btn = btnReset && record.checked == true) {
-    clearInterval(tick(Date.now()));
-  }
-}
-
-/* Timer */
-
-const timeIntervals = [
-  /* all in milliseconds */
-  ['hour', 3600000],
-  ['minute', 60000],
-  ['second', 1000]
-];
-
-const tick = start => () => {
-  let elapsed = Date.now() - start;
-  let newRecord = document.querySelector('#new-record');
-
-  for (let [unit, ms] of timeIntervals) {
-    let timing = Math.floor(elapsed / ms);
-    let timerParam = document.getElementById(unit);
-    let recordParam = document.getElementsByClassName(unit);
-
-    if (typeof timing == 'number') {
-      // 00-09 or 10-59 numbers range
-      timing < 10
-        ? (timerParam.textContent = `0${timing}`)
-        : (timerParam.textContent = `${timing}`);
-    }
-
-    /* if (recordParam == '--') {
-      newRecord.style.display = 'table-cell';
-      newRecord.innerHTML = 'NEW RECORD !!!';
-    } */
-
-    elapsed %= ms;
-  }
-};
 
 /* STEP 3: Generating 81 digits array for the game */
 
-function Gamedigits() {
-  /* 'sortedArray' is the first (base) 9-elements array
-   and the most important part of the all code */
-  let sortedArray = [];
+let sortedArray = []; // 'sortedArray' is the base 9-elements array
 
-  function sorting() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let n = numbers.length;
-    for (let i = 1; i <= n; ) {
-      let a = Math.floor(Math.random() * n);
-      let b = numbers[a];
+function sorting() {
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let n = numbers.length;
+  for (let i = 1; i <= n; ) {
+    let a = Math.floor(Math.random() * n);
+    let b = numbers[a];
 
-      sortedArray.push(b);
-      numbers.splice(a, 1);
+    sortedArray.push(b);
+    numbers.splice(a, 1);
 
-      n--;
-    }
-
-    return sortedArray;
+    n--;
   }
 
-  //  Make 27 part of small arrays
-  let fullArray = [];
+  return sortedArray;
+}
 
-  function arrayParts() {
-    sorting();
+let fullArray = []; //  Make 27 part of small arrays
 
-    let [i1, i2, i3, ...others1] = sortedArray;
-    let [, , , i4, i5, i6, ...others2] = sortedArray;
-    let [, , , , , , i7, i8, i9] = sortedArray;
+function arrayParts() {
+  sorting();
 
-    let part1 = [i1, i2, i3];
-    let part2 = [i4, i5, i6];
-    let part3 = [i7, i8, i9];
+  let [i1, i2, i3, ...others1] = sortedArray;
+  let [, , , i4, i5, i6, ...others2] = sortedArray;
+  let [, , , , , , i7, i8, i9] = sortedArray;
 
-    /* All other part(4-9) are made from part(1-3) by moving the last element in array to begin (like [a,b,c] -> [c,a,b] -> [b,c,a]). */
-    let part4 = [i9, i7, i8]; let part5 = [i3, i1, i2]; let part6 = [i6, i4, i5];
-    let part7 = [i5, i6, i4]; let part8 = [i8, i9, i7]; let part9 = [i2, i3, i1];
+  let part1 = [i1, i2, i3];
+  let part2 = [i4, i5, i6];
+  let part3 = [i7, i8, i9];
 
-    /* The right order of inserting every part into 9x9 sudoku array (fullArray) */
-    fullArray = [...part1, ...part2, ...part3,
-                ...part4, ...part5, ...part6,
-                ...part7, ...part8, ...part9,
-                ...part8, ...part9, ...part7,
-                ...part2, ...part3, ...part1,
-                ...part5, ...part6, ...part4,
-                ...part6, ...part4, ...part5,
-                ...part9, ...part7, ...part8,
-                ...part3, ...part1, ...part2];
+  /* All other part(4-9) are made from part(1-3) by moving the last element in array to begin (like [a,b,c] -> [c,a,b] -> [b,c,a]). */
+  let part4 = [i9, i7, i8]; let part5 = [i3, i1, i2]; let part6 = [i6, i4, i5];
+  let part7 = [i5, i6, i4]; let part8 = [i8, i9, i7]; let part9 = [i2, i3, i1];
 
-    return fullArray;
-  }
+  /* The right order of inserting every part into 9x9 sudoku array (fullArray) */
+  fullArray = [...part1, ...part2, ...part3,
+              ...part4, ...part5, ...part6,
+              ...part7, ...part8, ...part9,
+              ...part8, ...part9, ...part7,
+              ...part2, ...part3, ...part1,
+              ...part5, ...part6, ...part4,
+              ...part6, ...part4, ...part5,
+              ...part9, ...part7, ...part8,
+              ...part3, ...part1, ...part2];
 
+  return fullArray;
+}
+
+arrayParts();
+
+/* Checking correct sum for all 81 digits and 2 main diagonals before put digits on the board */
+function testBoard() {
   arrayParts();
-
-  //  Check correct sum for all digits and 2 main diagonals before inside them on the board
   let sumDigits = fullArray.reduce(function(previousValue, currentValue) {
     return previousValue + currentValue;
   });
@@ -173,23 +171,21 @@ function Gamedigits() {
   }
 
   if (sumDigits != 405 || leftResult != 45 || rightResult != 45) {
-    alert('Wrong digits!');
+    return ;
   }
-
-  return fullArray;
 }
+
+testBoard();
 
 
 /* STEP 4: Fill the board with the digits */
-let sudoku = Gamedigits();
 
 let uniqueIndexesArray = [];
-let indexNumbers = [];
-
-let indexArray = Array.from(sudoku); // copy of 'sudoku' to make the operations on it
+let indexArray = Array.from(fullArray); // copy of 'fullArray' to make the operations on it
 
 function cleanedArray(n) {
   let tempArray = [];
+  let indexNumbers = [];
   let k = 0;
 
   do {
@@ -204,58 +200,56 @@ function cleanedArray(n) {
   return uniqueIndexesArray;
 }
 
-let smallArray = cleanedArray(n);
+cleanedArray(n);
+
 
 /* Inserting white/empty area to input digits into the board */
-let noRepeatIndexes = Array.from(smallArray.values());
-let square = document.querySelectorAll('.squares');
 
 function fill() {
   for (let i = 0; i < n; i++) {
-    square[noRepeatIndexes[i]].insertAdjacentHTML('afterbegin', `<input type="text" class="digits" 
-    id="pos${noRepeatIndexes[i]}" maxlength="1" size="2" value="">`);
+    square[uniqueIndexesArray[i]].insertAdjacentHTML('afterbegin', `<input type="text" class="digits" 
+    id="pos${uniqueIndexesArray[i]}" maxlength="1" size="2" value="">`);
   }
 }
 
-fill();
+console.log(fullArray, indexArray);
+console.log(uniqueIndexesArray);
 
 /* Board is filled with digits and some of them are already covered beyond the user sight */
 
 function fillBoard() {
   let cellDigits = document.querySelectorAll('.squares > span'); // define space to put 81 digits
-
-  for (let i = 0; i < indexArray.length; i++) {
-    cellDigits[i].innerHTML = `${indexArray[i]}`;
-  }
+    for (let i = 0; i < indexArray.length; i++) {
+      cellDigits[i].innerHTML = `${indexArray[i]}`;
+    }
 }
 
 fillBoard();
 
 
+/* Checking if the entered digit matches the digit in the same index position in the sudoku */
 
 const getNumber = str => Number(str.match(/\d+/)[0]);
 const num = event => getNumber(event.target.id);
 
-document.querySelector('.gamespace').addEventListener('click', compareDigit);
-
-/* Checking if the entered digit matches the digit in the same index position in the sudoku */
+game.addEventListener('click', compareDigit);
 
 function compareDigit(event) {
   let digit = num(event);
   let digitSquare = document.querySelector(`#${event.target.id}`);
   let enteredValue = digitSquare.value;
-  let arrayDigit = sudoku[digit];
+  let arrayDigit = fullArray[digit];
 
   /* Input area displays blue/red color after good/bad answer respectively */
   if (digitSquare != null && enteredValue != '' && arrayDigit != '') {
     if (enteredValue == arrayDigit) {
       digitSquare.style.backgroundColor = 'rgb(110, 144, 247)';
       /* good answer will block the square permanently and make font color black (as it was default) */
-      digitSquare.disabled = true;
+      digitSquare.disabled == true;
       digitSquare.style.color = 'black';
       n--;
       
-      if (n == 0) {
+      if (n === 0) {
         GameEnd();
       }
     }
@@ -306,8 +300,8 @@ function optionsClear() {
   listReset();
 
   /* default graphic on the board */
-  document.querySelector('.example').style.background =
-    'url(/example-mini.svg)';
+  game.style.background = 'url(/example-mini.svg) no-repeat';
+  game.style.backgroundSize = 'cover';
 }
 
 /** User can hide theoretical description of "Sudoku". 
